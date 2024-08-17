57852814a42bf6a539259ea8ae38775c21e9aa46
Brad Flood <hey@bradflood.com>
2024-08-17 21:22:40 +1000

    docs tweak

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://rnap.dev',
+  url: 'https://rnap.dev/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
