12304ebf8d9231056630b85bdc9c93b350f9206d
Brad Flood <hey@bradflood.com>
2024-08-19 18:21:37 +1000

    disble hooks

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as AudioPro from './audioPro';
 
 export * from './constants';
-export * from './hooks';
+// export * from './hooks';
 export * from './interfaces';
 
 export default AudioPro;
